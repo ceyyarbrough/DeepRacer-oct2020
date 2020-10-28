76ecc1eabac7d24f997cb4b213d2511153a49cf8
Chris Yarbrough <christopher.e.yarbrough@gmail.com>
2020-10-28 12:30:48 -0400

    Update carmodel.py

--- carmodel.py
+++ carmodel.py
@@ -14,27 +14,10 @@ def reward_function(params):
 	left = params['is_left_of_center']
 
 
-	
-	
-	# Calculate 3 markers that are at varying distances away from the center line
-	marker_1 = 0.1 * track_width
-	marker_2 = 0.25 * track_width
-	marker_3 = 0.5 * track_width
-
 	#etc variables
 	CRASH_DETECTION = crash
 	ABD_STEERING_THERSHOLD = 12.5
 	DISTANCE_FROM_BORDER = 0.5 * track_width - distance_from_center
-	
-	# Give higher reward if the car is closer to center line and vice versa
-	if distance_from_center <= marker_1:
-		reward = 1.0
-	elif distance_from_center <= marker_2:
-		reward = 0.5
-	elif distance_from_center <= marker_3:
-		reward = 0.1
-	else:
-		reward = 1e-3  # likely crashed/ close to off track
 
 	#Keeps all wheels on track
 	if all_wheels_on_track:
@@ -57,6 +40,8 @@ def reward_function(params):
 		reward = 1.0
 	elif steering < 0 and left:
 		reward *= 0.8
+	elif steering == 0 and track_width = 0.5:
+		reward = 1.0
 	else:
 		reward = 1.0
 
